3ae091dbb6c79421b2464dcffc9a07b4db884f76
YukiUmetsu <yuuki.umetsu@gmail.com>
2021-02-11 20:22:12 -0600

    added reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,31 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let sign = Math.sign(n) // if number is negative, it gives -1, if pisitive 1, 0 -> 0
+    let numStrAbsolute = (n * sign).toString().split('').reverse().join('')
+    return sign * parseInt(numStrAbsolute)
+}
+
+function reverseInt2(n) {
+    if (n === 0) {
+        return 0;
+    }
+    let finalStr = '';
+    if (n < 0) {
+        finalStr = '-';
+        coefficient = -1;
+    } else {
+        coefficient = 1;
+    }
+    let numStrAbs = `${coefficient * n}`
+    for (let i = numStrAbs.length - 1; i >= 0; i--) {
+        let digit = numStrAbs[i]
+        if (digit !== '0') {
+            finalStr += digit
+        }
+    }
+    return parseInt(finalStr)
+}
 
 module.exports = reverseInt;
