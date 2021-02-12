96cedd0b9794045220e8d5d79ecb585ffe8df50f
YukiUmetsu <yuuki.umetsu@gmail.com>
2021-02-11 20:46:07 -0600

    added maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charMax = '';
+    const counters = str.split('').reduce((accumulator, currentValue) => {
+        accumulator[currentValue] = accumulator[currentValue] + 1 || 1;
+        return accumulator;
+    }, {})
+    Object.keys(counters).reduce((maxNum, currentKey) => {
+        if (counters[currentKey] > maxNum) {
+            charMax = currentKey;
+            return counters[currentKey];
+        }
+        return maxNum;
+    }, 0)
+    return charMax;
+}
 
 module.exports = maxChar;
