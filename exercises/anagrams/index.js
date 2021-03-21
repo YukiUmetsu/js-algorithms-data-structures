9c77b7d737a50db0f409350099f1f145f82518f9
YukiUmetsu <yuuki.umetsu@gmail.com>
2021-03-20 20:57:58 -0500

    anagram practice

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return filterAndSortString(stringA) === filterAndSortString(stringB);
+}
+
+function anagrams1(stringA, stringB) {
+    const mapA = createLetterMap(stringA);
+    const mapB = createLetterMap(stringB);
+    const keysA = Object.keys(mapA);
+    const keysB = Object.keys(mapB);
+
+    if (keysA.length !== keysB.length) {
+        return false
+    }
+    for (const keyElm in mapA) {
+        if (mapA[keyElm] != mapB[keyElm]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function filterAndSortString(givenStr) {
+    return givenStr.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function createLetterMap(givenStr) {
+    let letterMap = {};
+    givenStr
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .map(letter => {
+        if (!letterMap[letter]) {
+            letterMap[letter] = 1;
+        } else {
+            letterMap[letter] += 1;
+        }
+    });
+    return letterMap;
+}
 
 module.exports = anagrams;
